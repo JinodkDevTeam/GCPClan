b73029f8e81dc5eb39b0022f521e17a559bb1253
Ngọc Lam <33188123+NgLamVN@users.noreply.github.com>
2022-01-14 10:46:21 +0700

    API4 changes can cleanup.
    It take me an hour to complete this fking bad plugin. At least thanks PHPStorm for code reformater and inspection...

--- src/instantlyta/fcaclan/UpdateTopClanTask.php
+++ src/instantlyta/fcaclan/UpdateTopClanTask.php
@@ -1,15 +1,13 @@
 <?php
-
+declare(strict_types=1);
 
 namespace instantlyta\fcaclan;
 
 
 use pocketmine\scheduler\Task;
 
-class UpdateTopClanTask extends Task
-{
-    public function onRun($currentTick)
-    {
-        FCAClan::getInstance()->updateTopClan();
-    }
+class UpdateTopClanTask extends Task{
+	public function onRun() : void{
+		FCAClan::getInstance()->updateTopClan();
+	}
 }
--- src/instantlyta/fcaclan/event/ClanChangeEvent.php
+++ src/instantlyta/fcaclan/event/ClanChangeEvent.php
@@ -1,21 +1,17 @@
 <?php
-
+declare(strict_types=1);
 
 namespace instantlyta\fcaclan\event;
 
 
 use pocketmine\event\player\PlayerEvent;
-use pocketmine\Player;
+use pocketmine\player\Player;
 
-class ClanChangeEvent extends PlayerEvent
-{
-    public static $handlerList = null;
+class ClanChangeEvent extends PlayerEvent{
+	protected string $clanName;
 
-    protected $clanName;
-
-    public function __construct(Player $player, string $clanName)
-    {
-        $this->player = $player;
-        $this->clanName = $clanName;
-    }
+	public function __construct(Player $player, string $clanName){
+		$this->player = $player;
+		$this->clanName = $clanName;
+	}
 }
